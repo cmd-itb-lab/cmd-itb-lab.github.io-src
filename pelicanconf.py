c0ad28910f1952e36af5e935205ffa4ea58764f4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-08-20 15:01:56 +0700

    Add ITB and TF links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('ITB', 'https://itb.ac.id/'),
+         ('Teknik Fisika ITB', 'http://python.org/'),
+        )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['html']
